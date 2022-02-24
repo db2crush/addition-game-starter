1e61e81432833c683d8d0d112c5383b5ac798d9e
db2crush <db2crush@gmail.com>
2022-02-24 19:20:26 +0900

    feat: addition game running on klaytn baobab

--- truffle.js
+++ truffle.js
@@ -1,1 +1,17 @@
 // truffle.js config for klaytn.
+const PrivateKeyConnector = require('connect-privkey-to-provider')
+const NETWORK_ID = '1001'
+const GASLIMIT = '20000000'
+const URL = 'https://api.baobab.klaytn.net:8651'
+const PRIVATE_KEY = '0xcaf8adbe3491ac45b4647aefdafc44f3d7ae01bd6d48ba365486c88ae595cdbc'
+
+module.exports = {
+    networks: {
+        klaytn: {
+            provider: new PrivateKeyConnector(PRIVATE_KEY, URL),
+            network_id: NETWORK_ID,
+            gas: GASLIMIT,
+            gasPrice: null,
+        }
+    }
+}
